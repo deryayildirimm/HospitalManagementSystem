2e430015cca7ffe10ab541b12fee8f25bd3deac8
zeynep <zeynep203551@hotmail.com>
2024-12-19 16:02:49 +0300

    physical finding entity

---
+++ src/Pusula.Training.HealthCare.Domain.Shared/Treatment/Examinations/PhysicalFindings/PhysicalFindingConsts.cs
@@ -0,0 +1,10 @@
+﻿namespace Pusula.Training.HealthCare.Treatment.Examinations.PhysicalFindings;
+
+public class PhysicalFindingConsts
+{
+    public const int WeightMaxValue = 500;
+    public const int HeightMaxValue = 300;
+    
+    public const int WeightMinValue = 0;
+    public const int HeightMinValue = 10;
+}
